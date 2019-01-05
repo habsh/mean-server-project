7cb7ac726fb2d6219a47f8ec0eb447bb047e3a69
Thomas <bichuk@gmail.com>
2019-01-05 10:33:59 -0600

    minor updates and db script

--- server/routes/employees.js
+++ server/routes/employees.js
@@ -11,7 +11,7 @@ router.get('/', function (req, res, next) {
 });
 
 router.get('/viewLeave', function (req, res, next) {
-  var id = 1;
+  var id = 3;
   //var query = Employee.findById(id);
   // Employee.find({ empId: id }, function (err, doc) {
   //   if (err) return res.status(500).json({ error: err });
@@ -45,7 +45,7 @@ router.post('/applyLeave', function (req, res) {
     leaveStatus: 'Pending'
   };
 
-  Employee.findOne({ empId: id }, function (err, doc) {
+  Employee.findOne({ empId: id }, function (err, doc) {console.log(doc)
     if (err) return res.status(500).json({ error: err });
     else {
 
@@ -55,37 +55,21 @@ router.post('/applyLeave', function (req, res) {
         }
         doc.appliedLeaves.push(leave);
       }
+      //Return error if leave balance is 0
+      if (doc.leaveBalance <= 0) {
+        return res.status(500).json({ error: 'Your leave balance is 0!' });
+      }
 
-      if (doc.leaveBalance != undefined && doc.leaveBalance != null)
+      if (doc.leaveBalance != undefined && doc.leaveBalance != null) {
         doc.leaveBalance = doc.leaveBalance == 0 ? 0 : doc.leaveBalance - 1;
-
+        
+      }
       doc.save(function (err, doc) {
         if (err) return res.json({ error: err });
         else return res.status(200).json({ msg: 'Successfully saved!!' });
       });
     }
   });
-
-  // Employee.find({ empId: id }).exec(function (err, doc) {
-  //   if (err) return res.status(500).json({ error: err });
-  //   else {
-
-  //     if (leave!= undefined && leave != null){
-  //       if (!Array.isArray(doc.appliedLeaves)) {
-  //         doc.appliedLeaves = [];
-  //     }
-  //       doc.appliedLeaves.push(leave);
-  //     }
-
-  //     if (doc.leaveBalance != undefined && doc.leaveBalance != null) 
-  //       doc.leaveBalance = doc.leaveBalance - 1;
-  //       console.log(doc);
-  //     doc.save(function (err, doc) {
-  //       if (err) return res.json({ error: err });
-  //       else return res.status(200).json({ msg: 'Successfully saved!!' });
-  //     });
-  //   }
-  // });
 });
 
 module.exports = router;
